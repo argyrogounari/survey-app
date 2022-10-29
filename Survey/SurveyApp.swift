b67c1f9d11d3a20124928ae64e0362e769461e5a
argyrogounari <914947@swansea.ac.uk>
2022-10-29 12:08:17 +0300

    Initial Commit

---
+++ Survey/SurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyApp.swift
+//  Survey
+//
+//  Created by argyro gounari on 29/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
