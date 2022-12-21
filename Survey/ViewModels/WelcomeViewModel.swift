55833e48dea045da038fa232dd10c5c0c75d2c63
argyro gounari <agounari@trading-point.com>
2022-12-21 16:39:04 +0200

    start: add TCA for TabView

--- Survey/ViewModels/WelcomeViewModel.swift
+++ Survey/ViewModels/WelcomeViewModel.swift
@@ -10,27 +10,49 @@ import UIKit
 import SwiftUI
 
 struct AppState {
+    // - MARK: Welcome
     var isShowingDetailView = false
+    
+    // - MARK: TabView
+    var numQuestionsSubmitted = 0
+    var currentQuestion = 1
+    var isPreviousButtonDisabled = true
+    var isNextButtonDisabled = false
+    var questions: [Question] = []
+    
+    var tabViewState: TabViewState {
+        get {
+            TabViewState(
+                numQuestionsSubmitted: self.numQuestionsSubmitted,
+                currentQuestion: self.currentQuestion,
+                isPreviousButtonDisabled: self.isPreviousButtonDisabled,
+                isNextButtonDisabled: self.isNextButtonDisabled,
+                questions: self.questions
+            )
+        }
+        set {
+            self.numQuestionsSubmitted = newValue.numQuestionsSubmitted
+            self.currentQuestion = newValue.currentQuestion
+            self.isPreviousButtonDisabled = newValue.isPreviousButtonDisabled
+            self.isNextButtonDisabled = newValue.isNextButtonDisabled
+            self.questions = newValue.questions
+        }
+    }
 }
 
-final class Store<Value, Action>: ObservableObject {
-    let reducer: (inout Value, Action) -> Void
-    @Published var value: Value
-    
-    init(initialValue: Value, reducer: @escaping (inout Value, Action) -> Void) {
-        self.reducer = reducer
-        self.value = initialValue
-    }
-    
-    func send(_ action: Action) {
-        self.reducer(&self.value, action)
-    }
+struct TabViewState {
+    var numQuestionsSubmitted: Int
+    var currentQuestion: Int
+    var isPreviousButtonDisabled: Bool
+    var isNextButtonDisabled: Bool
+    var questions: [Question]
 }
 
 // - MARK: Actions
 
 enum AppAction {
     case welcome(WelcomeAction)
+    case tabView(TabViewAction)
     
     var welcome: WelcomeAction? {
        get {
@@ -42,17 +64,35 @@ enum AppAction {
          self = .welcome(newValue)
        }
      }
+    
+    var tabView: TabViewAction? {
+       get {
+         guard case let .tabView(value) = self else { return nil }
+         return value
+       }
+       set {
+         guard case .tabView = self, let newValue = newValue else { return }
+         self = .tabView(newValue)
+       }
+     }
 }
 
 enum WelcomeAction {
     case welcomeTapped
 }
 
+enum TabViewAction {
+    case previousTapped
+    case nextTapped
+    case questionOnDisplayChanged // is this action? maybe effect?
+    case tabViewAppeared
+}
+
 // - MARK: Reducers
 
 let appReducer: (inout AppState, AppAction) -> Void = combine(
     pullback(welcomeReducer, value: \.isShowingDetailView, action: \.welcome),
-    welcomeReducerTest
+    pullback(tabViewReducer, value: \.tabViewState, action: \.tabView)
 )
 
 func welcomeReducer(state: inout Bool, action: WelcomeAction) {
@@ -62,11 +102,22 @@ func welcomeReducer(state: inout Bool, action: WelcomeAction) {
     }
 }
 
-func welcomeReducerTest(state: inout AppState, action: AppAction) {
-//    switch action {
-//    case .welcome(.welcomeTapped):
-////        state = true
-//    }
+func tabViewReducer(state: inout TabViewState, action: TabViewAction) {
+    switch action {
+    case .previousTapped:
+        state.currentQuestion -= 1
+    case .nextTapped:
+        state.currentQuestion += 1
+    case .questionOnDisplayChanged:
+        state.isPreviousButtonDisabled = state.currentQuestion == 1
+        state.isNextButtonDisabled = state.currentQuestion == state.questions.last?.id
+    case .tabViewAppeared:
+        Task {
+            await Database().getQuestions{ questionsList in
+//                state.questions = questionsList
+            }
+        }
+    }
 }
 
 func combine<Value, Action> (
@@ -87,5 +138,19 @@ action: WritableKeyPath<GlobalAction, LocalAction?>
     return { globalValue, globalAction in
         guard let localAction = globalAction[keyPath: action] else {return}
         reducer(&globalValue[keyPath: value], localAction)
+    }
+}
+
+final class Store<Value, Action>: ObservableObject {
+    let reducer: (inout Value, Action) -> Void
+    @Published var value: Value
+    
+    init(initialValue: Value, reducer: @escaping (inout Value, Action) -> Void) {
+        self.reducer = reducer
+        self.value = initialValue
+    }
+    
+    func send(_ action: Action) {
+        self.reducer(&self.value, action)
     }
 }
